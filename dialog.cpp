2451054fc1063f30199cdc29d867d7aca2e60e26
Will Whitten <103906785+WillWhitten127@users.noreply.github.com>
2023-07-14 07:43:09 -0500

    Update dialog.cpp
    Updated to include QuickWidget.

--- dialog.cpp
+++ dialog.cpp
@@ -1,19 +1,33 @@
+// dialog.cpp
+
 #include "dialog.h"
 #include "./ui_dialog.h"
+#include <QQuickView>
+#include <QQuickWidget>
+#include <QQmlContext>
 
-Dialog::Dialog(QWidget *parent)
+Dialog::Dialog(UserProfile* userProfile, CarProfile* carProfile, LicenseProfile* licenseProfile, QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::Dialog)
+    , m_userProfile(userProfile)
+    , m_carProfile(carProfile)
+    , m_licenseProfile(licenseProfile)
 {
     ui->setupUi(this);
+    QQuickView view;
+    QQmlContext *context = ui->quickWidget->rootContext();
+
+
+    context->setContextProperty("userProfile", m_userProfile);
+    context->setContextProperty("carProfile", m_carProfile);
+    context->setContextProperty("licenseProfile", m_licenseProfile);
+
+    view.setSource(QUrl::fromLocalFile("main.qml"));
+    view.show();
+
 }
 
 Dialog::~Dialog()
 {
     delete ui;
-}
-
-void Dialog::ChangeText(){
-    ui->lineEdit->setText("will");
-
 }
